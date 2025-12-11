6094d1df7a89a034d5e48d7cf153e6433a3365fb
jensmorten <jens.morten.nilsen@gmail.com>
2025-12-11 18:45:05 +0100

    work

--- compeval/evaluate.py
+++ compeval/evaluate.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 import os
 import io
 
-pred_path = "predictions_helt_på_jordet.csv"
+pred_path = "predictions_MarcusRockyRidge.csv"
 fasit_enc_path = "test_fasit.enc"
 
 
